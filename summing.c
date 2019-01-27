e66daedf4f007e9b3f75599830fc1fd55a3c156f
demetyildiz <45262944+demetyildiz@users.noreply.github.com>
2019-01-28 00:21:58 +0300

    Add files via upload

---
+++ summing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//adding two integer and printing n the screen by Demet Yýldýz
+
+int main(){
+	
+	int x=5; //initializing integer x and y with their values
+	int y=3;
+	int sum;// initializing the summation of x and y
+	sum=x+y;
+	
+	printf("%d",sum);
+	
+	
+	return 0;
+	
+	
+}
